d1fcca340c58314082303dac4c46f79e866dfc5c
aggresss <aggresss@163.com>
2020-12-16 21:57:49 +0800

    2020-12-16 21:57:49 CST W50D3

---
+++ workbench/multithreading/src/main/java/cn/router7/multithreading/App.java
@@ -0,0 +1,13 @@
+package cn.router7.multithreading;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
