b47304945bbd68661935c468991eb942065ce648
aggresss <aggresss@163.com>
2020-12-22 15:32:45 +0800

    2020-12-22 15:32:45 CST W51D2

--- design-patterns/creational/singleton/src/main/java/cn/router7/singleton/App.java
+++ design-patterns/creational/singleton/src/main/java/cn/router7/singleton/App.java
@@ -7,12 +7,45 @@ import org.slf4j.LoggerFactory;
  * Hello world!
  *
  */
-public class App
-{
+public class App {
 
     private static final Logger log = LoggerFactory.getLogger(App.class);
-    public static void main( String[] args )
-    {
-        log.info( "Hello World!" );
+
+    /**
+     * Program entry point.
+     *
+     * @param args command line args
+     */
+    public static void main(String[] args) {
+
+        // eagerly initialized singleton
+        var ivoryTower1 = IvoryTower.getInstance();
+        var ivoryTower2 = IvoryTower.getInstance();
+        log.info("ivoryTower1={}", ivoryTower1);
+        log.info("ivoryTower2={}", ivoryTower2);
+
+        // lazily initialized singleton
+        var threadSafeIvoryTower1 = ThreadSafeLazyLoadedIvoryTower.getInstance();
+        var threadSafeIvoryTower2 = ThreadSafeLazyLoadedIvoryTower.getInstance();
+        log.info("threadSafeIvoryTower1={}", threadSafeIvoryTower1);
+        log.info("threadSafeIvoryTower2={}", threadSafeIvoryTower2);
+
+        // enum singleton
+        var enumIvoryTower1 = EnumIvoryTower.INSTANCE;
+        var enumIvoryTower2 = EnumIvoryTower.INSTANCE;
+        log.info("enumIvoryTower1={}", enumIvoryTower1);
+        log.info("enumIvoryTower2={}", enumIvoryTower2);
+
+        // double checked locking
+        var dcl1 = ThreadSafeDoubleCheckLocking.getInstance();
+        log.info(dcl1.toString());
+        var dcl2 = ThreadSafeDoubleCheckLocking.getInstance();
+        log.info(dcl2.toString());
+
+        // initialize on demand holder idiom
+        var demandHolderIdiom = InitializingOnDemandHolderIdiom.getInstance();
+        log.info(demandHolderIdiom.toString());
+        var demandHolderIdiom2 = InitializingOnDemandHolderIdiom.getInstance();
+        log.info(demandHolderIdiom2.toString());
     }
 }
---
+++ design-patterns/creational/singleton/src/main/java/cn/router7/singleton/InitializingOnDemandHolderIdiom.java
@@ -0,0 +1,26 @@
+package cn.router7.singleton;
+
+public final class InitializingOnDemandHolderIdiom {
+
+    /**
+     * Private constructor.
+     */
+    private InitializingOnDemandHolderIdiom() {
+    }
+
+    /**
+     * Singleton instance.
+     *
+     * @return Singleton instance
+     */
+    public static InitializingOnDemandHolderIdiom getInstance() {
+        return HelperHolder.INSTANCE;
+    }
+
+    /**
+     * Provides the lazy-loaded Singleton instance.
+     */
+    private static class HelperHolder {
+        private static final InitializingOnDemandHolderIdiom INSTANCE = new InitializingOnDemandHolderIdiom();
+    }
+}
